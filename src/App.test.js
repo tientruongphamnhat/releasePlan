db56cfaac4b975500079e09551eaa4c952b1fd6c
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-06-07 23:53:44 +0700

    update no test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
