b5acb20d1bb4ef06ff8b060482c9561a640fe29c
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-06-07 23:28:08 +0700

    try circleCI

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
